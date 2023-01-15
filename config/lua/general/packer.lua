9b733f107ec151b50d40514f0c5219968be6073d
BrunoTarijon <btarijon@gmail.com>
2023-01-15 16:26:46 -0300

    add quickscope

--- config/lua/general/packer.lua
+++ config/lua/general/packer.lua
@@ -10,4 +10,5 @@ return require('packer').startup(function(use)
   use { "ellisonleao/gruvbox.nvim" }
   use ('nvim-treesitter/nvim-treesitter', {run =  ':TSUpdate'})
   use 'mbbill/undotree'
+  use 'unblevable/quick-scope'
 end)
