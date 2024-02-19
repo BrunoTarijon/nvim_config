91a0e3de888cc5587a37467ba2209e978472494d
BrunoTarijon <btarijon@gmail.com>
2024-02-19 09:38:28 -0300

    add gopls

--- config/after/plugin/golang-lsp.lua
+++ config/after/plugin/golang-lsp.lua
@@ -14,7 +14,9 @@ lspconfig.gopls.setup {
     },
   },
 }
-local on_attach = function(client, bufnr)
-  -- Enable completion triggered by <c-x><c-o>
-  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
-end
+-- local on_attach = function(client, bufnr)
+--   -- Enable completion triggered by <c-x><c-o>
+--   vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+--   vim.api.nvim_buf_set_keymap(bufnr, "n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
+--   vim.api.nvim_buf_set_keymap(bufnr, "n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
+-- end
