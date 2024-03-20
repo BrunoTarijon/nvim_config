6e5bff03f04ca623a7a78fdbf26aed4a2619756a
BrunoTarijon <btarijon@gmail.com>
2024-03-20 15:00:02 -0300

    Sync

--- config/lua/general/set.lua
+++ config/lua/general/set.lua
@@ -36,8 +36,8 @@ vim.api.nvim_create_autocmd("CursorHold", {
 
 vim.o.expandtab = true -- expand tab input with spaces characters
 vim.o.smartindent = true -- syntax aware indentations for newline inserts
-vim.o.tabstop = 4 -- num of space characters per tab
-vim.o.shiftwidth = 4 -- spaces per indentation level
+vim.o.tabstop = 2 -- num of space characters per tab
+vim.o.shiftwidth = 2 -- spaces per indentation level
 
 -- Use tabs
 -- vim.o.noexpandtab = true
